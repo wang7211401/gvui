8de4ee77fdfa0dadc9daab511ba1a9289b29b03a
wang7211401 <wang7211401@163.com>
2020-10-29 17:56:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hello world</div>
+<Main></Main>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Main
+    }
+}
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+    第一个组件
+</div>
+</template>
