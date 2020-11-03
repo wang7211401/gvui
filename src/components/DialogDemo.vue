d64c0e1176732087367fe84d2d7f929cd87f97f6
wang7211401 <wang7211401@163.com>
2020-11-03 15:44:16 +0800

    支持 visiable 属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,48 @@
 <template>
-<div>
-    Dialog 文档
-</div>
+<div>Dialog 示例</div>
+<h1>示例一</h1>
+<Button @click="toggleVisiable">显示Dialog</Button>
+<Dialog v-model:visiable="dialogVisiable" :cancel="cancelFn" :ok="okFn" />
+
+<h1>示例二(取消点击遮罩层关闭)</h1>
+<Button @click="toggleVisiable">显示Dialog</Button>
+<Dialog v-model:visiable="dialogVisiable" :closeOnClickOverlay="false" :cancel="cancelFn" :ok="okFn" />
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue"
+import Button from "../lib/Button.vue"
+import {
+    ref
+} from 'vue'
+export default {
+    components: {
+        Dialog,
+        Button
+    },
+    setup() {
+        const dialogVisiable = ref(false)
+        const toggleVisiable = () => {
+            dialogVisiable.value = true
+        }
+        const okFn = () => {
+            console.log('关闭2')
+
+            dialogVisiable.value = false
+            return false
+        }
+        const cancelFn = () => {
+            console.log(1)
+            console.log(2)
+            console.log('关闭')
+            dialogVisiable.value = false
+        }
+        return {
+            dialogVisiable,
+            toggleVisiable,
+            okFn,
+            cancelFn
+        }
+    }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,152 @@
+<template>
+<template v-if="visiable">
+    <div class="gvui-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="gvui-dialog-wrapper">
+        <div class="gvui-dialog">
+            <header>标题 <span class="gvui-dialog-close" @click="colse"></span></header>
+            <main>内容</main>
+            <footer>
+                <Button @click="ok">确定</Button>
+                <Button theme="text" @click="cancel">取消</Button>
+            </footer>
+        </div>
+    </div>
+</template>
+</template>
+
+<script>
+import Button from "./Button.vue"
+export default {
+    components: {
+        Button
+    },
+    props: {
+        visiable: {
+            type: Boolean,
+            default: false
+        },
+        closeOnClickOverlay: {
+            type: Boolean,
+            default: true
+        },
+        ok: {
+            type: Function
+        },
+        cancel: {
+            type: Function
+        }
+    },
+    setup(props, context) {
+        const colse = () => {
+            console.log(context)
+            console.log({
+                ...props
+            })
+            context.emit('update:visiable', false)
+        }
+        const onClickOverlay = () => {
+            props.closeOnClickOverlay ? colse() : ""
+        }
+        const ok = () => {
+            console.log("ok")
+            if (props.ok?.() !== false) {
+                close()
+            }
+        }
+        const cancel = () => {
+            console.log({
+                ...context.attrs
+            })
+            console.log({
+                ...props
+            })
+            props.cancel && props.cancel()
+            // context.emit('cancel')
+            // close()
+        }
+        return {
+            colse,
+            onClickOverlay,
+            ok,
+            cancel
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+
+.gvui-dialog {
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 15em;
+    max-width: 90%;
+
+    &-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: fade_out(black, 0.5);
+        z-index: 10;
+    }
+
+    &-wrapper {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 11;
+    }
+
+    >header {
+        padding: 12px 16px;
+        border-bottom: 1px solid $border-color;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 20px;
+    }
+
+    >main {
+        padding: 12px 16px;
+    }
+
+    >footer {
+        border-top: 1px solid $border-color;
+        padding: 12px 16px;
+        text-align: right;
+    }
+
+    &-close {
+        position: relative;
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        cursor: pointer;
+
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            height: 1px;
+            background: black;
+            width: 100%;
+            top: 50%;
+            left: 50%;
+        }
+
+        &::before {
+            transform: translate(-50%, -50%) rotate(-45deg);
+        }
+
+        &::after {
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
+    }
+}
+</style>
