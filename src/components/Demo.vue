c8b48deb79c9984e9c6ea8028f981d16ea6005cf
wang7211401 <wang7211401@163.com>
2020-11-05 16:37:56 +0800

    代码高亮

---
+++ src/components/Demo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="toggleCode">查看代码</Button>
+    </div>
+    <div class="demo-code" v-if="codeVisiable">
+      <pre class="language-html" v-html="codeHtml"></pre>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+    computed,
+    ref
+} from 'vue'
+import Button from "../lib/Button.vue"
+import "prismjs"
+import "prismjs/themes/prism.css"
+const Prism = (window as any).Prism
+
+export default {
+    props:{
+        component:Object
+    },
+    components: {
+        Button
+    },
+    setup(props) {
+        const codeVisiable = ref(false)
+        const toggleCode = ()=>{
+            codeVisiable.value = !codeVisiable.value
+        }
+
+        const codeHtml = computed(()=>{
+            return Prism.highlight(props.component.__sourceCode,Prism.languages.html,'html')
+        })
+
+        return {
+            codeVisiable,
+            toggleCode,
+            Prism,
+            codeHtml
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    $border-color:#d9d9d9;
+    .demo{
+        border: 1px solid $border-color;
+        margin: 16px 0 32px;
+        >h2 {
+            font-size: 20px;
+            padding: 8px 16px;
+            border-bottom: 1px solid $border-color;
+        }
+        &-component {
+            padding: 16px;
+        }
+        &-actions {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+        }
+        &-code {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+            >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+            }
+        }
+    }
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,82 +1,28 @@
 <template>
 <div>
   <h1>Switch 组件示例 </h1>
-  <div class="demo">
-    <h2>{{Switch1Demo.__sourceCodeTitle}}</h2>
-    <div class="demo-component">
-      <component :is="Switch1Demo" />
-    </div>
-    <div class="demo-actions">
-      <Button @click="toggle1 = !toggle1">查看代码</Button>
-    </div>
-    <div class="demo-code" v-if="toggle1">
-      <pre>{{Switch1Demo.__sourceCode}}</pre>
-    </div>
-  </div>
-  <div class="demo">
-   <h2>{{Switch2Demo.__sourceCodeTitle}}</h2>
-    <div class="demo-component">
-      <component :is="Switch1Demo" />
-    </div>
-    <div class="demo-actions">
-      <Button @click="toggle2 = !toggle2">查看代码</Button>
-    </div>
-    <div class="demo-code" v-if="toggle2">
-       <pre>{{Switch2Demo.__sourceCode}}</pre>
-    </div>
-  </div>
+    <Demo :component="Switch1Demo" />
+    <Demo :component="Switch2Demo" />
 </div>
 </template>
 
 <script lang="ts">
 import {
+    computed,
     ref
 } from 'vue'
 import Switch1Demo from "./Switch1.demo.vue"
 import Switch2Demo from "./Switch2.demo.vue"
-console.log(Switch1Demo.__sourceCode)
-import Button from "../lib/Button.vue"
+import Demo from "./Demo.vue"
 export default {
     components: {
-        Button
+        Demo
     },
     setup() {
-        const toggle1 = ref(false)
-        const toggle2 = ref(false)
         return {
-            toggle1,
-            toggle2,
             Switch1Demo,
-            Switch2Demo,
+            Switch2Demo
         }
     }
 }
 </script>
-<style lang="scss" scoped>
-    $border-color:#d9d9d9;
-    .demo{
-        border: 1px solid $border-color;
-        margin: 16px 0 32px;
-        >h2 {
-            font-size: 20px;
-            padding: 8px 16px;
-            border-bottom: 1px solid $border-color;
-        }
-        &-component {
-            padding: 16px;
-        }
-        &-actions {
-            padding: 8px 16px;
-            border-top: 1px dashed $border-color;
-        }
-        &-code {
-            padding: 8px 16px;
-            border-top: 1px dashed $border-color;
-            >pre {
-            line-height: 1.1;
-            font-family: Consolas, 'Courier New', Courier, monospace;
-            margin: 0;
-            }
-        }
-    }
-</style>
